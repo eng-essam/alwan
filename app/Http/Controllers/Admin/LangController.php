2826706446215d13e042ee479a0844b431004c0f
eng-essam <essamhamdy425@gmail.com>
2022-11-20 15:47:57 +0200

    push

---
+++ app/Http/Controllers/Admin/LangController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+
+class LangController extends Controller{
+    public function index()
+    {
+        //
+    }
+}
